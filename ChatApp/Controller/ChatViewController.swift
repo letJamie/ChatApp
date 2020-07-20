f8b65a530e28df101d5721c9357ff56aa7348359
letJamie <stillbut@naver.com>
2020-07-20 15:06:12 +0900

    Add login, Logout function

--- ChatApp/Controller/ChatViewController.swift
+++ ChatApp/Controller/ChatViewController.swift
@@ -6,20 +6,38 @@
 //  Copyright © 2020 Jamie. All rights reserved.
 //
 import UIKit
-class ChatViewController: UIViewController {
+import Firebase
 
+class ChatViewController: UIViewController {
+    
     @IBOutlet weak var tabelView: UITableView!
     
     @IBOutlet weak var messageTextfield: UITextField!
     
     
     override func viewDidLoad() {
-         super.viewDidLoad()
-         // Do any additional setup after loading the view.
-     }
+        super.viewDidLoad()
+        title = "⚡️FlashChat"
+        navigationItem.hidesBackButton = true
+        // Do any additional setup after loading the view.
+    }
     
     
     @IBAction func sendPressed(_ sender: UIButton) {
     }
+    
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+        
+        
+    }
+    
     
 }
--- ChatApp/Controller/LoginViewController.swift
+++ ChatApp/Controller/LoginViewController.swift
@@ -6,13 +6,30 @@
 //  Copyright © 2020 Jamie. All rights reserved.
 //
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
-   
+    
     @IBAction func loginPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            
+            Auth.auth().signIn(withEmail: email, password: password) {authResult, error in
+                
+                if let e = error {
+                    print(e)
+                    
+                } else {
+                    
+                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                    
+                }
+            }
+        }
+        
     }
     
 }
